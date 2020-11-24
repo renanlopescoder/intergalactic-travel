99c1cb7c823770d5de8a6418221b3348800f0cc0
Renan Lopes <renanlopescoder@gmail.com>
2020-11-24 13:26:59 +0100

    feat(cors): config cors vercel config

--- next.config.js
+++ next.config.js
@@ -1,24 +1,3 @@
 const withImages = require("next-images");
 
-module.exports = withImages({
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "*",
-          },
-        ],
-      },
-    ];
-  },
-});
+module.exports = withImages({});
